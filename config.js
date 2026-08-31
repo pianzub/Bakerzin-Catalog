f484c28c23a8f7193316b82a8b3dda1a0044ea9d
pianzub <pianzub@gmail.com>
2026-08-31 19:23:48 +0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-  // Ganti dengan URL deployment Web App Apps Script yang berakhiran /exec.
-  API_URL: 'https://script.google.com/macros/s/AKfycby8hFklR4lY9V9OcnvEGd-yQ_ncBQzpWj_9Dm3DGxtgeBO_qGOlJ1qCMAMu2KYCaAAF/exec'
-};
